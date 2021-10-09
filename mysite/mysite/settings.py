c430d9cdbaec55a658f0b975a08b18ac1e720cff
Daman Raaj <damanraajsuryavanshi@yahoo.in>
2021-10-09 14:06:46 +0530

    Updated gitignore. Changed test credentials. Moved the credentials to .env file.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -18,9 +18,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-reodghgd@znn-n91t$^#kk=y1noh^aj5atxhp6f(h+z=vqvm38'
+with open(".env") as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
